e873190476d5d9d59e18a667362e57d382a9c29e
Yadavendra Sakharkar <prajwal1619@gmail.com>
2025-03-28 22:10:25 +0530

    setting up the frontend and react-router

---
+++ frontend/creditRating_FE/src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Mortgages from "./Components/Mortgages";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Mortgages />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/creditRating_FE/src/Components/Mortgages.tsx
@@ -0,0 +1,8 @@
+const Mortgages: React.FC = () => {
+  return (
+    <div className="MortgageRecord">
+      <h1>This will show all the mortgage records we have submitted so far</h1>
+    </div>
+  );
+};
+export default Mortgages;
